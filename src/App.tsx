1b916c33bc13433350bc25cce4c475fe3a635f13
BILLY Ronico <Ronico.BILLY@akka.eu>
2023-02-20 19:25:03 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,115 @@
+import { useMachine } from '@xstate/react'
+import { useEffect } from 'react'
+import { machine } from './machine'
+
+const App = (): JSX.Element => {
+  const [state, send] = useMachine(machine)
+  const { question, answers, flag, indexResponded, indexAnswer, points } = state.context
+  useEffect(() => {
+    send('LOAD')
+  }, [send])
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center">
+      <div className="flex flex-col gap-y-2">
+        <p className="text-2xl font-bold uppercase tracking-wide text-white">Country quiz</p>
+        <div className="relative flex h-[450px] w-[350px] flex-col justify-evenly rounded-xl bg-white px-4 py-4">
+          {state.matches('loading') && (
+            <div className="flex items-center justify-center">
+              <p className="animate-bounce text-sm font-bold text-[#2F527B]">Loading...</p>
+            </div>
+          )}
+          {state.matches('error') && (
+            <div className="flex items-center justify-center">
+              <p className="text-sm font-bold text-[#EA8282]">Something went wrong</p>
+            </div>
+          )}
+          {state.matches('game') && (
+            <>
+              <img
+                src="/undraw_adventure.svg"
+                alt="winner svg"
+                className="absolute top-0 right-0 w-24 -translate-y-1/2"
+              />
+              <div className="flex h-1/6 items-center">
+                {flag !== null && <img src={flag} alt="flag" className="w-[80px] shadow" />}
+              </div>
+              <p className="flex h-1/6 items-center text-sm font-bold text-[#2F527B]">{question}</p>
+              <div className="flex h-1/2 flex-col justify-around">
+                {answers.map((a, i) => {
+                  const className = (() => {
+                    if (state.matches({ game: 'win' })) {
+                      if (indexResponded === i) {
+                        return '!bg-[#60BF88] !text-white !hover:bg-[#60BF88] !hover:text-white !border-[#60BF88]'
+                      } else {
+                        return 'hover:bg-white hover:text-[#6066D0] hover:text-opacity-80 hover:border-[#6066D0]'
+                      }
+                    } else if (state.matches({ game: 'lose' })) {
+                      if (i === indexResponded) {
+                        return '!bg-[#EA8282] !text-white !hover:bg-[#EA8282] !hover:text-white !border-[#EA8282]'
+                      } else if (i === indexAnswer) {
+                        return '!bg-[#60BF88] !text-white !hover:bg-[#60BF88] !hover:text-white !border-[#60BF88]'
+                      } else {
+                        return 'hover:bg-white hover:text-[#6066D0] hover:text-opacity-80 hover:border-[#6066D0]'
+                      }
+                    }
+                    return ''
+                  })()
+                  return (
+                    <button
+                      key={i}
+                      className={
+                        'flex gap-x-6 truncate rounded-lg border border-[#6066D0] border-opacity-80 bg-white px-4 py-2 text-left text-sm font-medium text-[#6066D0] text-opacity-80 transition-colors hover:border-white hover:bg-[#F9A826] hover:text-white ' +
+                        className
+                      }
+                      onClick={() =>
+                        send({
+                          type: 'PLAY',
+                          indexResponded: i
+                        })
+                      }>
+                      <span>{String.fromCharCode(65 + i)}</span> <span>{a}</span>
+                    </button>
+                  )
+                })}
+              </div>
+              <div className="flex h-1/6 items-center">
+                {[{ game: 'win' }, { game: 'lose' }].some(state.matches) && (
+                  <button
+                    className="ml-auto rounded bg-[#F9A826] px-6 py-2 font-bold text-white"
+                    onClick={() => send('NEXT')}>
+                    Next
+                  </button>
+                )}
+              </div>
+            </>
+          )}
+          {state.matches('score') && (
+            <div className="flex flex-col items-center justify-center gap-y-6">
+              <img src="/undraw_winners.svg" alt="winner svg" className="mb-auto w-36" />
+              <div className="flex flex-1 flex-col items-center gap-y-2">
+                <p className="text-2xl font-bold text-[#2F527B]">Results</p>
+                <p className="flex items-center gap-x-2 text-sm font-bold tracking-wide text-[#2F527B]">
+                  You got <span className="text-2xl text-[#60BF88]">{points}</span> correct answers.
+                </p>
+                <button
+                  className="mt-4 rounded bg-[#F9A826] px-6 py-2 font-bold text-white"
+                  onClick={() => send('TRY_AGAIN')}>
+                  Try again
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+      <p className="absolute bottom-2 text-xs font-medium text-[#F2F2F2]">
+        created by{' '}
+        <a href="https://devchallenges.io/portfolio/BillyRonico412" className="font-bold underline">
+          BillyRonico412
+        </a>{' '}
+        - devChallenges.io
+      </p>
+    </div>
+  )
+}
+
+export default App
---
+++ src/machine.ts
@@ -0,0 +1,208 @@
+import { assign, createMachine, DoneInvokeEvent } from 'xstate'
+import { z } from 'zod'
+
+export const zodCountrie = z.object({
+  name: z.object({
+    common: z.string()
+  }),
+  flags: z.object({
+    svg: z.string()
+  }),
+  capital: z.optional(z.array(z.string()))
+})
+
+interface MachineContext {
+  allCountries: z.infer<typeof zodCountrie>[] | null
+  points: number
+  questionType: 0 | 1 | null
+  question: string | null
+  flag: string | null
+  answers: string[]
+  indexAnswer: number | null
+  indexResponded: number | null
+}
+
+type MachineEvent =
+  | {
+      type: 'LOAD'
+    }
+  | {
+      type: 'START'
+    }
+  | {
+      type: 'PLAY'
+      indexResponded: number
+    }
+  | {
+      type: 'NEXT'
+    }
+  | {
+      type: 'TRY_AGAIN'
+    }
+
+const machine =
+  /** @xstate-layout N4IgpgJg5mDOIC5QFsCGBjAFgSwHZgDpsIAbMAYgBkB5AQQBEBtABgF1FQAHAe1mwBds3XBxAAPRABYAzAA4CAdgCMcgKzMlzSc1kBOWdIA0IAJ6IATJPlyFM3dNULdCx6oC+b42ix5CJbqgQeFDkEMKEeABu3ADWhN44+AT+gcEIUdzoqILCLKx5ojx8OSJI4oi6SgBsBKq66pJKjY1OksZmCObmCgTVOnUqVXI6Hl4YiX4BQbghYABOc9xzBJwk2QBmS8gECb7JU2kZWSV5BWVFAkKloBIITrqKkt1DjuaaSubtiEoK5gTOLVk5lkv1UklsoxAuyS80WcyodCYbEKvEuwlEtxk8mUckkTy6liUSi+CB+SgI4MaenMDns-Uh0MIUFQyEIq1QJmC5AACpRaABNM5cVElDGIMHSAjMOTSSSqIH6bSqElgh7SaRdEFgqqNZhVBnjPbM1kELBgdAxLlCkAXUVlW5OcmaHRPewKFyyEngyWqKpVargvV1ZQGnxJY2EM0Wq1KdjnEVXMV3FxSrpVSq6GmyJRAlWyZiKcyqVTqp7qn66UMTAgRggAdzw5AAcgBRAAaABVrbbE-bEH7VLVZKoc9JpUHZV6urUPlpzP7qrp-VWjSzJrAKK3O92E+i+wh6n8VPpZFUtES-STbIOS9qT28hgoV0lYOglhQOwAlfkAfVoAHFaAASSbHdil7G5EGkP1ajpQMR3VN4rzlWoHEkextHqeV3EhXBuAgOBREZFFwL3SDOkaAhoPnJoVA1McHBJcwCwcHMDDHZw3kkZ8IlIMASLRa5yk6PQqKqGjGkQhjlVMCwrCokFGnlMd5U0fVPChQ0khSaYoAEu1yLqQcql+Is-T1BQ9CYywCFkRSVM0MFqnUsYw0IWEln0iDhKGB5fL0Mc3lUQkSU0HphxzKp83sSwnhczS3JrNcvLI4TgTVZwFDkGlAskKoSXVQcNGY7QXF9aQlBw1zq1rdlORmFKhNudUemkTLsvVZhLHy2SEBLSUmjeZQzyUSppR4pKTSjS0GvjUimv7MECCGZiRzy8z3RVX5anlXRpUUok9Am2sGyEntUtuXRKLs1b6iy35mF0FUtGWqpfSLewpKUY6132DdGqTK7yRu5gjPu5int6xx5GcYsmmLYK2srDTGQIV93wB-c3sHDV7qeHQXBkZCbzQ9MbuqaQnw8NwgA */
+  createMachine<MachineContext, MachineEvent>(
+    {
+      id: 'machine',
+      initial: 'idle',
+      context: {
+        allCountries: null,
+        points: 0,
+        questionType: null,
+        question: null,
+        flag: null,
+        answers: [],
+        indexAnswer: null,
+        indexResponded: null
+      },
+      states: {
+        idle: {
+          on: {
+            LOAD: 'loading'
+          }
+        },
+
+        loading: {
+          invoke: {
+            src: 'loadCountries',
+            onDone: {
+              target: 'game',
+              actions: assign((_, event: DoneInvokeEvent<z.infer<typeof zodCountrie>[]>) => ({
+                allCountries: event.data
+              }))
+            },
+            onError: {
+              target: 'error'
+            }
+          }
+        },
+
+        error: {
+          on: {
+            LOAD: 'loading'
+          }
+        },
+
+        game: {
+          initial: 'playing',
+          states: {
+            playing: {
+              entry: 'setQuestion',
+              on: {
+                PLAY: {
+                  target: 'checking',
+                  actions: assign((_, event) => ({
+                    indexResponded: event.indexResponded
+                  }))
+                }
+              }
+            },
+
+            checking: {
+              always: [
+                {
+                  cond: (context) => {
+                    return context.indexAnswer === context.indexResponded
+                  },
+                  actions: assign((context) => ({
+                    points: context.points + 1
+                  })),
+                  target: 'win'
+                },
+                {
+                  cond: (context) => {
+                    return context.indexAnswer !== context.indexResponded
+                  },
+                  target: 'lose'
+                }
+              ]
+            },
+
+            win: {
+              on: {
+                NEXT: 'playing'
+              }
+            },
+
+            lose: {
+              on: {
+                NEXT: '#machine.score'
+              }
+            }
+          }
+        },
+
+        score: {
+          on: {
+            TRY_AGAIN: 'game'
+          }
+        }
+      }
+    },
+    {
+      services: {
+        loadCountries: async (): Promise<z.infer<typeof zodCountrie>[]> => {
+          const allCountriesRes = await fetch('https://restcountries.com/v3.1/all')
+          const allCountries = await allCountriesRes.json()
+          return z.array(zodCountrie).parse(allCountries)
+        }
+      },
+      actions: {
+        setQuestion: assign((context) => {
+          if (context.allCountries !== null) {
+            const questionType = Math.floor(Math.random() * 2)
+            const indexCountry = Math.floor(Math.random() * context.allCountries.length)
+            const country = context.allCountries[indexCountry]
+            if (questionType === 0 && country.capital !== undefined) {
+              const question = `${country.capital[0]} is capital of`
+              const indexAnswer = Math.floor(Math.random() * 4)
+              const answers: string[] = []
+              for (let i = 0; i < 4; i++) {
+                if (i === indexAnswer) {
+                  answers.push(country.name.common)
+                } else {
+                  const indexOtherCountry = Math.floor(Math.random() * context.allCountries.length)
+                  if (indexOtherCountry === indexCountry) {
+                    i--
+                    continue
+                  }
+                  const country = context.allCountries[indexOtherCountry]
+                  answers.push(country.name.common)
+                }
+              }
+              return {
+                question,
+                answers,
+                indexAnswer,
+                questionType,
+                flag: null
+              }
+            } else {
+              const question = `Which country does this flag belong to?`
+              const flag = country.flags.svg
+              const indexAnswer = Math.floor(Math.random() * 4)
+              const answers: string[] = []
+              for (let i = 0; i < 4; i++) {
+                if (i === indexAnswer) {
+                  answers.push(country.name.common)
+                } else {
+                  const indexOtherCountry = Math.floor(Math.random() * context.allCountries.length)
+                  if (indexOtherCountry === indexCountry) {
+                    i--
+                    continue
+                  }
+                  const country = context.allCountries[indexOtherCountry]
+                  answers.push(country.name.common)
+                }
+              }
+              return { question, answers, indexAnswer, flag }
+            }
+          }
+          return {}
+        })
+      }
+    }
+  )
+
+export { machine }
